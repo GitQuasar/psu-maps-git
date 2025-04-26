640ef9d6342dd0219dd21801d7df030b94895a0c
GitQuasar <cursedonela@yandex.ru>
2025-04-26 18:44:30 +0500

    WC Button added

---
+++ src/components/molecules/wcButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import BaseButton from '../atoms/baseButton';
+
+const WcButton = () => {
+    return <BaseButton iconSrc={require('../images/wc_icon.png')} />;
+};
+
+export default WcButton;
